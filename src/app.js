a36653fa3d47db2ff61060eec4fa1ed0bada7646
Nikola Prijovic <nikolaprijovic@Nikolas-MacBook-Air.local>
2022-01-09 21:28:52 +0100

    Added google books

--- src/app.js
+++ src/app.js
@@ -5,18 +5,19 @@ import config from "./config.js";
 
 const bot = new Telegraf(config.telegram_token);
 
-const url = `https://imdb-api.com/en/API/Search/${config.imdb_token}/`;
+const movies_url = `https://imdb-api.com/en/API/Search/${config.imdb_token}/`;
+const books_url = "https://www.googleapis.com/books/v1/volumes?q="
 
 bot.start((ctx) => ctx.reply("Hello"));
 bot.help((ctx) => ctx.reply("Help message"));
 
 bot.on("text", (ctx) => {
-  const userInput = ctx.message.text;
+  const userInput = ctx.message.text.toLowerCase();
 
   if (userInput.toLowerCase().includes("#gledaj")) {
-    const movie_name = userInput.toLowerCase().split("#gledaj")[1].trim();
+    const movie_name = userInput.split("#gledaj")[1].trim();
 
-    fetch(url + movie_name)
+    fetch(movies_url + movie_name)
       .then((res) => res.json())
       .then((data) => {
         if (data.results[0]?.image) {
@@ -24,6 +25,22 @@ bot.on("text", (ctx) => {
         }
       });
   }
+
+  if (userInput.toLowerCase().includes("#citaj")) {
+    const book_name = userInput.split("#citaj")[1].trim();
+
+    fetch(books_url + book_name)
+      .then((res) => res.json())
+      .then((data) => {
+        ctx.reply(data.items[0].volumeInfo.imageLinks.thumbnail);
+        ctx.reply(data.items[0].volumeInfo.description);
+      });
+  }
 });
+
+bot.telegram.getMe().then((botInfo) => {
+  bot.options.username = botInfo.username
+})
+
 
 bot.launch();
